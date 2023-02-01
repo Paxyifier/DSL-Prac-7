9adba833e4087ba41d2154599f35d03e514a3ef2
Paxyifier <59118083+Paxyifier@users.noreply.github.com>
2023-02-01 15:22:43 +0000

    singly done

--- src/singlyLinkedList.cpp
+++ src/singlyLinkedList.cpp
@@ -21,25 +21,27 @@ class Node{
         return this->next !=NULL;
     }
 };
-class CircularLinkedList{
+class LinkedList{
     protected:
     Node *head;
     public:
-    CircularLinkedList(){
+    LinkedList(){
         this->head = NULL;
     }
     void insertAtEnd(int data){
         Node *temp=this->head, *newNode = new Node(data);
         if (this->head ==NULL){
             this->head = newNode;
+            return;
         }
         while (temp->next != NULL) {
             temp = temp->next;
         }
-        temp->setNext(newNode);   
+        temp->next = newNode;   
     }
-    void insertAtFront(int data){
-        this->head = new Node(data, this->head);
+    void insertAtStart(int data){
+        Node *newNode= new Node(data, this->head);
+        this->head = newNode;
     }
     bool insertAt(int data, int pos){
         Node *temp=this->head, *newNode = new Node(data);
@@ -51,8 +53,8 @@ class CircularLinkedList{
         while (temp->next != NULL && pos>iter ) {
             iter++;
             if (iter == pos){
-                newNode->setNext(temp);
-                temp->setNext(newNode);
+                newNode->next = temp->next;
+                temp->next = newNode;
                 return true;
             }
             temp = temp->next;
@@ -73,7 +75,7 @@ class CircularLinkedList{
             if (pos==iter){
                 deletedData = temp->next->data;
                 free(temp->next);
-                temp->setNext(temp->next->next);  
+                temp->next =(temp->next->next);  
                 return deletedData; 
             }
             temp=temp->next;
@@ -96,13 +98,16 @@ class CircularLinkedList{
             cout << "List is Empty"<<endl;
             return -1;
         }
-        while (temp->next->next != NULL){
+        while (temp->next != NULL){
             temp=temp->next;
+            if (temp->next == NULL){
+                deletedData = temp->data;
+                free(temp);
+                temp->next = NULL;
+                return deletedData;
+            }
         }
-        deletedData = temp->next->data;
-        free(temp->next);
-        temp->setNext(temp->next->next);
-        return deletedData;
+        return -1;
     }
     int count(){
         Node *temp = this->head;
@@ -116,25 +121,34 @@ class CircularLinkedList{
     void print(){
         Node *temp = this->head;
         cout << "\nLinked List Elements: ";
-        while(temp->next != NULL){
+        while(temp != NULL){
             cout << temp->data ;
-            temp=temp->next;
-            if (temp->next != NULL){
+            temp = temp->next;
+            if (temp != NULL){
                 cout << " -> ";
-            }
+            } 
         }
-        cout<<endl;
+        cout << endl;
     }
     void reverse(){
-        Node *cur, *prev, *next;
-        cur = this->head;
-        while(cur->next != NULL){
-            next = cur->next;
-            cur->setNext(prev);
-            prev = cur;
-            cur = next;
+        Node *prev = NULL, *current = this->head, *next = NULL;
+        while (current != NULL){
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
         this->head = prev;
+        
+        // Node *cur, *prev, *next;
+        // cur = this->head;
+        // while(cur->next != NULL){
+        //     next = cur->next;
+        //     cur->next =(prev);
+        //     prev = cur;
+        //     cur = next;
+        // }
+        // this->head = prev;
     }
     int search(int q){
         Node *temp = this->head;
@@ -147,3 +161,93 @@ class CircularLinkedList{
         return -1;
     }
 };
+
+int main(){
+    bool run = false;
+    int choice, input, pos;
+    LinkedList list;
+    while (run) {
+        cout << "Singly Linked List Program"<<endl;
+        cout << "1.\tInsert at front"<<endl;
+        cout << "2.\tInsert at end"<<endl;
+        cout << "3.\tInsert at specific position"<<endl;
+        cout << "4.\tDelete from front"<<endl;
+        cout << "5.\tDelete from end"<<endl;
+        cout << "6.\tDelete from specific position"<<endl;
+        cout << "7.\tSearch"<<endl;
+        cout << "8.\tReverse"<<endl;
+        cout << "9.\tPrint"<<endl;
+        cout << "10.\tExit"<<endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch(choice){
+            case 1:
+                cout << "Enter the element to be inserted: ";
+                cin >> input;
+                list.insertAtStart(input);
+                break;
+            case 2:
+                cout << "Enter the element to be inserted: ";
+                cin >> input;
+                list.insertAtEnd(input);
+                break;
+            case 3:
+                cout << "Enter the element to be inserted: ";
+                cin >> input;
+                cout << "Enter the position: ";
+                cin >> pos;
+                if (list.insertAt(input, pos)){
+                    cout << "Element inserted successfully"<<endl;
+                }else{
+                    cout << "Element not inserted"<<endl;
+                }
+                break;
+            case 4:
+                if (list.removeFromStart() != -1){
+                    cout << "Element deleted successfully"<<endl;
+                }else{
+                    cout << "Element not deleted"<<endl;
+                }
+                break;
+            case 5:
+                if (list.removeFromEnd() != -1){
+                    cout << "Element deleted successfully"<<endl;
+                }else{
+                    cout << "Element not deleted"<<endl;
+                }
+                break;
+            case 6:
+                cout << "Enter the position: ";
+                cin >> pos;
+                if (list.removeFrom(pos) != -1){
+                    cout << "Element deleted successfully"<<endl;
+                }else{
+                    cout << "Element not deleted"<<endl;
+                }
+                break;
+            case 7:
+                cout << "Enter the element to be searched: ";
+                cin >> input;
+                if (list.search(input) != -1){
+                    cout << "Element found"<<endl;
+                }else{
+                    cout << "Element not found"<<endl;
+                }
+                break;
+            case 8:
+                list.reverse();
+                cout << "List reversed successfully"<<endl;
+                break;
+            case 9:
+                list.print();
+                break;
+            case 10:
+                run = false;
+                break;
+            default:
+                cout << "Invalid choice"<<endl;
+                break;
+        }
+    }
+    return 0;
+}
